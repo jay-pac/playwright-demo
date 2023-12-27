f540c8613f12bff26bebc997e3259e0d9aaaf7eb
Jason Pacitti <jpacitti@jasons-air.lan>
2023-12-27 15:35:00 -0600

    basic playwright tests

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,96 @@
+const { test, expect } = require("@playwright/test");
+
+test.skip("Browser Fixture Playwright test", async ({ browser }) => {
+  //playwright code
+
+  // creates a new instance of the browser
+  const context = await browser.newContext(); // if no injection of cookies is needed, playwright page fixtures creates a new instance
+  const page = await context.newPage();
+
+  const userName = page.locator("#username");
+  const passWord = page.locator("#password");
+  const signIn = page.locator("#signInBtn");
+  const cardTitles = page.locator(".card-body a");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  await userName.fill("rahulshetty");
+  await passWord.fill("learning");
+  await signIn.click();
+  const errorMessage = await page.locator("[style*='block']").textContent();
+  console.log(errorMessage);
+  await expect(page.locator("[style*='block']")).toContainText(
+    "Incorrect username/password."
+  );
+
+  await userName.fill("");
+  await userName.fill("rahulshettyacademy");
+  await signIn.click();
+  //    await passWord.fill('learning');
+  console.log(await cardTitles.first().textContent());
+  console.log(await cardTitles.nth(1).textContent());
+
+  // extract all the phone titles and store into a single variable.  Returns an array
+  const allTitles = await cardTitles.allTextContents();
+  console.log(allTitles);
+});
+
+test("UI Controls", async ({ browser, page }) => {
+  const userName = page.locator("#username");
+  const passWord = page.locator("#password");
+  const signIn = page.locator("#signInBtn");
+  const documentLink = page.locator('[href*="documents-request"]');
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const dropDown = page.locator("select.form-control");
+  await dropDown.selectOption("consult");
+
+  //select a radio button
+  await page.locator(".radiotextsty").last().click();
+  await page.locator("#okayBtn").click();
+  // await page.pause()
+
+  //asseration
+  await expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+  // useful isChecked() method if you are trying to get a boolean value
+  console.log(await page.locator(".radiotextsty").last().isChecked());
+
+  await page.locator("#terms").click();
+  await expect(page.locator("#terms")).toBeChecked();
+
+  //uncheck the check box
+  await page.locator("#terms").uncheck();
+  expect(await page.locator("#terms").isChecked()).toBeFalsy();
+
+  await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+
+test("Handling Child Windows and Tabs", async ({ browser }) => {
+  
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const userName = page.locator("#username");
+
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator('[href*="documents-request"]');
+
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    documentLink.click(),
+  ]);
+  const text = await newPage.locator(".red").textContent();
+  const textArray = text.split("@");
+  const domain = textArray[1].split(" ")[0];
+  console.log(domain);
+
+  await userName.fill(domain);
+});
+
+// record and play back script
+test.skip('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.goto('https://www.google.com/search?q=rahul+shetty+acaedemy&oq=rahul+shetty+acaedemy&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTIwNjUzajBqNKgCALACAA&sourceid=chrome&ie=UTF-8');
+  await page.getByRole('link', { name: 'Rahul Shetty Academy:' }).click();
+  await page.locator('div:nth-child(2) > .inner > .icon-box > .icon').click();
+  await expect(page.getByRole('link', { name: 'NEW Learning paths' })).toBeVisible();
+});
---
+++ tests/register-user.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require("@playwright/test");
+
+test("", async ({ browser, page }) => {
+  // page fixture gives you a default browser instance
+  await page.goto("https://rahulshettyacademy.com/client/");
+  // get title
+  console.log(await page.title());
+  await page.locator(".text-reset").click();
+  await page.locator("#firstName").fill("James");
+  await page.locator("#lastName").fill("Kirk");
+  await page.locator("#userEmail").fill("jtest@mailinator.com");
+  await page.locator("#userMobile").fill("5105555555");
+  await page.locator("#userPassword").fill("T3st12345");
+  await page.locator("#confirmPassword").fill("T3st12345");
+  await page.locator('[type="checkbox"]').click();
+  await page.locator("#login").click();
+  await page.waitForTimeout(5000);
+});
